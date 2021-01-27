9400ec42eea21496b4716eccf4db5ab9740eee7d
nagendra <nagendra.patel@yaantra.com>
2021-01-27 13:20:14 +0530

    Initial project setup

---
+++ NKP/NKP.h
@@ -0,0 +1,18 @@
+//
+//  NKP.h
+//  NKP
+//
+//  Created by YAANTRA on 25/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NKP.
+FOUNDATION_EXPORT double NKPVersionNumber;
+
+//! Project version string for NKP.
+FOUNDATION_EXPORT const unsigned char NKPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKP/PublicHeader.h>
+
+
---
+++ NKPExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NKPExample
+//
+//  Created by YAANTRA on 27/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
