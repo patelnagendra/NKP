a2a258a06cf9ab6b2317e8d70722510a2a36f320
nagendra <nagendra.patel@yaantra.com>
2021-01-27 17:19:59 +0530

    license add

---
+++ NKP/NKP.swift
@@ -0,0 +1,21 @@
+//
+//  NKP.swift
+//  NKP
+//
+//  Created by YAANTRA on 27/01/21.
+//
+
+import Foundation
+public final class NKPLib {
+
+    let name = "NKPLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
